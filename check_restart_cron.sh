6d6c5db8b13d84c80cc77516f0f7831f67ef2c72
John de Graaff <john@de-graaff.net>
2025-03-07 14:14:46 +0100

    some

---
+++ check_restart_cron.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#= check_restart_cron.sh
+# (c)202% John@de-Graaff.net
+#
+
+
+## JDG-NOTE: also copy this file (see inside for instructions): ./check_restart_cron.cron
+
+
+# display every line executed in this bash script:
+#set -o xtrace
+#
+BASENAME=`basename $0`
+echo "# running: $BASENAME ... "
+#SCRIPT=`realpath -s $0`  # man says: "-s, --strip, --no-symlinks : don't expand symlinks"
+#SCRIPT_PATH=`dirname $SCRIPT`
+#
+MYUID=$( id -u )
+#usage() {
+#  #echo "# usage: $BASENAME { req.flag | [ -opt.flag string ] } " 1>&2 
+#  echo "# usage: $BASENAME " 1>&2 
+#  exit 1
+#}
+echo_exit1() { echo $1 ; exit 1 ; }
+if [ $MYUID != 0 ]; then
+  # $* is a single string, whereas $@ is an actual array.
+  echo "# provide your password for 'sudo':" ; sudo "$0" "$@" ; exit 0 ;
+fi
+#
+LOGFILE="$HOME/check_restart_cron.log"
+DATE=`date +d%y%m%d-%Hh%Mm%Ss`
+#
+PIDFILE="/var/run/ledclock.pid"
+PID=$(cat $PIDFILE)
+echo "# PID (ledclock) = $PID"
+# test: PID="1234567"
+#
+if ps --pid $PID > /dev/null
+then
+  echo "# 'ps --pid' says: RUNNING "
+else
+  echo "# 'ps --pid' says: ABSENT "
+  echo "$DATE check_restart_cron.sh restarting ..." >> $LOGFILE
+  /usr/sbin/service ledclock restart
+fi
+#
+exit 0
+#
+#-eof
